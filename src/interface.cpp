f40b42d7a77854a4b821505ea05e7104fea6af30
brian-d-d-d <155209486+brian-d-d@users.noreply.github.com>
2026-05-07 19:29:27 +1000

    refactor: renamed command_string to command

--- src/interface.cpp
+++ src/interface.cpp
@@ -8,18 +8,19 @@
 void serial_receive() {
     while (1) {
         if (Serial.available() > 0) {
-            char command_string[Serial.available()];
+            char command[Serial.available() + 1];
             int index = 0;
             
             uint8_t data = Serial.read();
             while (Serial.available() > 0) {
-                command_string[index] = data;
+                command[index] = data;
                 data = Serial.read();
 
                 index++;
             }
 
-            handle_command_string(command_string, index);
+            command[index] = '\0';
+            handle_command(command, index + 1);
         }
     }
 }
@@ -28,23 +29,23 @@ void serial_transmit(uint8_t value) {
     Serial.write(value);
 }
 
-void handle_command_string(char command_string[], int len) {
-    switch (command_string[0]) {
+void handle_command(char command[], int len) {
+    switch (command[0]) {
         //Get command
         case 'g':
-            handle_get_command(command_string, len);
+            handle_get_command(command, len);
             break;
         //Set command
         case 's':
-            handle_set_command(command_string, len);
+            handle_set_command(command, len);
             break;
         default:
             break;
     }
 }
 
-void handle_get_command(char command_string[], int len) {
-    int bus_num = command_string[2] - '0';
+void handle_get_command(char command[], int len) {
+    int bus_num = command[2] - '0';
 
     serial_transmit(bus_get_byte_value(BUSES[bus_num], BUS_LENS[bus_num]));
 }
@@ -53,21 +54,21 @@ void handle_get_command(char command_string[], int len) {
 //Example for binary: s 1 0b111000
 //Example for hex: s 1 0xFFDD
 //Example for decimal: s 1 0d12
-void handle_set_command(char command_string[], int len) {
-    int bus_num = command_string[2] - '0';
+void handle_set_command(char command[], int len) {
+    int bus_num = command[2] - '0';
     uint8_t value;
 
     //Binary (0b111000)
-    if (memcmp(command_string + 4, "0b", 2) == 0) {
-        value = strtoul(command_string + 5, NULL, 2);
+    if (memcmp(command + 4, "0b", 2) == 0) {
+        value = strtoul(command + 5, NULL, 2);
     }
     //Hex (0xFFDD)
-    else if (memcmp(command_string + 4, "0x", 2) == 0) {
-        value = strtoul(command_string + 5, NULL, 16);
+    else if (memcmp(command + 4, "0x", 2) == 0) {
+        value = strtoul(command + 5, NULL, 16);
     }
     //Decimal number (0d12)
-    else if (memcmp(command_string + 4, "0d", 2) == 0) {
-        value = strtoul(command_string + 5, NULL, 10);
+    else if (memcmp(command + 4, "0d", 2) == 0) {
+        value = strtoul(command + 5, NULL, 10);
     }
     else {
         return;
